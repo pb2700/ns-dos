90f97a043de37103fb2d8a2adbd10f9ee654a98c
petar <>
2021-11-16 23:45:35 -0600

    Initial commit.

---
+++ js/lessonData.js
@@ -0,0 +1,35 @@
+export const lessonData = {
+
+  steps: [
+
+    {id: 2, text: 'System intializing...', target: 4, type: 'load'},
+    {id: 4, text: 'Enter username to begin:', target: 10, type: 'input'},
+    {id: 10, text: 'Hello username, welcome to NS-DOS 7.5.', target: 20, type: 'normal'},
+    {id: 20, text: 'This application will let you configure the system modules.', target: 40, type: 'normal'},
+    {id: 40, text: 'Select the system to configure:', targets: [
+      {text: 'Multi-Spectral Phase Inverter', target: 50},
+      {text: 'Secondary Warp Plasma Manifold', target: 60},
+      {text: 'Forward Iso-Linear Interlock 2.', target: 70}
+    ], type: 'option'},
+    {id: 50, text: 'Loading Multi-Spectral Phase Inverter to local memory...', target: 80, type: 'load'},
+    {id: 60, text: 'Loading Secondary Warp Plasma Manifold system to local memory...', target: 80, type: 'load'},
+    {id: 70, text: 'Loading Forward Iso-Linear Interlock 2 system to local memory...', target: 80, type: 'load'},
+    {id: 80, text: 'Loading complete.', target: 90, type: 'normal'},
+    {id: 90, text: 'Now choose an option to configure:', targets: [
+      {text: 'Reconfigure plasma stream', target: 100},
+      {text: 'Quantize matrix shield modulator', target: 110},
+      {text: 'Recompile spectral matrix processor', target: 120}
+    ], type: 'option'},
+    {id: 100, text: 'Reconfiguring plasma stream...', target: 130, type: 'load'},
+    {id: 110, text: 'Quantizing matrix shield modulator...', target: 140, type: 'load'},
+    {id: 120, text: 'Recompiling spectral matrix processor...', target: 150, type: 'load'},
+    {id: 130, text: 'Plasma stream has been reconfigured.', target: 160, type: 'normal'},
+    {id: 140, text: 'Matrix shield modulator has completed quatization.', target: 160, type: 'normal'},
+    {id: 150, text: 'Spectral Matrix Processor has completed recompiling.', target: 160, type: 'normal'},
+    {id: 160, text: 'You have succesfully configured the system.', target: 170, type: 'normal'},
+    {id: 170, text: 'The system will now begin shutdown.', target: 180, type: 'normal'},
+    {id: 180, text: 'System is shutting down...', target: 190, type: 'load'},
+    {id: 190, text: '', target: 0, type: 'end'},
+  ]
+
+}
---
+++ js/main.js
@@ -0,0 +1,103 @@
+import {lessonData} from './lessonData.js'
+let currentStep = 10
+let currentStepIndex = 0
+const dialogText = document.querySelector('.dialog_text')
+const mainWindow = document.querySelector('.window1')
+const startWindow = document.querySelector('.window2')
+const endScreen = document.querySelector('.end_screen')
+let userName = ''
+let nextStepIndex = 0
+
+
+const goNextStep = (stepIndex = null) =>{
+  if(stepIndex === null){
+    stepIndex = currentStepIndex
+  }
+  nextStepIndex = lessonData.steps.findIndex((element, index)=>{
+      if(element.id === lessonData.steps[stepIndex].target){
+        return true
+      }
+  })
+  displayStep(nextStepIndex)
+}
+
+async function displayStep(step){
+
+  currentStepIndex = step
+  const options = document.querySelector('.options')
+  const okButton = document.querySelector('.ok_button')
+  const loadBar = document.querySelector('.load_bar')
+
+  if(lessonData.steps[currentStepIndex].type === 'end'){
+    okButton.classList.add('hide');
+    options.classList.add('hide');
+    loadBar.classList.add('hide');
+    endScreen.classList.remove('hide');
+    return
+  }
+  if(lessonData.steps[currentStepIndex].type === 'load'){
+    mainWindow.classList.remove('hide');
+    okButton.classList.add('hide');
+    options.classList.add('hide');
+    loadBar.classList.remove('hide');
+    dialogText.innerHTML = lessonData.steps[currentStepIndex].text.replace('username', userName)
+    let loadDelay = new Promise((resolve, reject)=>{
+      setTimeout(()=>{resolve()}, 5000)
+    })
+    await loadDelay;
+    loadBar.classList.add('hide')
+    goNextStep()
+    return
+  }
+  if(lessonData.steps[currentStepIndex].type === 'option'){
+    okButton.classList.add('hide');
+    options.classList.remove('hide');
+    options.innerHTML = ''
+    lessonData.steps[currentStepIndex].targets.forEach((item, i)=>{
+      let option = document.createElement('div')
+      option.classList.add('button');
+      let optionText = document.createTextNode(item.text)
+      option.appendChild(optionText)
+      option.addEventListener("click", ()=>{clickOption(item.target)})
+      options.appendChild(option)
+    })
+    dialogText.innerHTML = lessonData.steps[currentStepIndex].text.replace('username', userName)
+    return
+  }
+  if(lessonData.steps[currentStepIndex].type === 'normal'){
+    mainWindow.classList.remove('hide');
+    options.classList.add('hide');
+    okButton.classList.remove('hide');
+    dialogText.innerHTML = lessonData.steps[currentStepIndex].text.replace('username', userName)
+    return
+  }
+  if(lessonData.steps[currentStepIndex].type === 'input'){
+    startWindow.classList.remove('hide');
+    options.classList.add('hide');
+    okButton.classList.remove('hide');
+    mainWindow.classList.add('hide');
+    dialogText.innerHTML = lessonData.steps[currentStepIndex].text.replace('username', userName)
+    return
+  }
+}
+
+const clickOption = (id) =>{
+  nextStepIndex = lessonData.steps.findIndex((element, index)=>{
+      if(element.id === id){
+        return true
+      }
+  })
+
+  displayStep(nextStepIndex)
+}
+
+const startGame = ()=>{
+  startWindow.classList.add('hide');
+  mainWindow.classList.remove('hide');
+  userName = document.querySelector('.login_input').value
+  displayStep(2)
+}
+
+document.querySelector(".submit_button").addEventListener("click", () => {startGame()})
+document.querySelector(".ok_button").addEventListener("click", () => {goNextStep()})
+displayStep(0)
